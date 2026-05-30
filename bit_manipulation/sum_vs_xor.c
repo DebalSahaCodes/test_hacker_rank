182b68c3e98639d3e13924cc130324486fcb0c54
Debal Saha <116924730+DebalSahaCodes@users.noreply.github.com>
2026-05-30 13:08:12 -0700

    correct explanation of the logic for optimized solution

--- bit_manipulation/sum_vs_xor.c
+++ bit_manipulation/sum_vs_xor.c
@@ -1,10 +1,6 @@
 /* Find all possible x in range [0,n]
 *  such that : x + n = x ^ n
-*
-*  
-*/
-
-
+**/
 long naive(long n)
 {
     int count = 0;
@@ -15,19 +11,25 @@ long naive(long n)
     return count;
 }
 
+/*
+* NOTE: optimized solution patttern
+* -------------------------------------
+* XOR is binary addition without carry.
+*  The condition of result_XOR = result_ADD
+*  happens when there is no carry.
+*  
+*  To achieve that for each number between
+*  0 and n, we need to check for all the bits
+*  in n that are 0 trailing the MSB.
+*/
 
 long optim(long n)
 {
-    // find the 0's before MSB
-    // say, n_zeros is count,
-    // and return all possible 
-    // combinations i.e. 
-    // 2-pow(n_zeros)
     
     long count = 0;
     long bit = log2(n); // get MSB
     
-    // now find all preceding 0's
+    // now find all 0's after MSB
     for(; bit >= 0L; --bit)
     {
         long mask = (1L << bit);
